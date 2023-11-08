97402c53c7232a63d6d56abbf5324d4b4c42fbf7
Kyle <berlinerkyle@gmail.com>
2023-11-07 22:17:39 -0700

    Completed Task 2. Created the compareGuesses() function.

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ let currentRoundNumber = 1;
 const generateTarget = () => {
     return Math.floor(Math.random() * 10);
 };
+
+// Compare the Computer and the User guess to the Secret Target Number Function
+
+const compareGuesses = (user, computer, target) => {
+    if (Math.abs(user - target) === Math.abs(computer - target)) {
+        return true;
+    } else if (Math.abs(user - target) < Math.abs(computer - target)) {
+        return true;
+    } else {
+        return false;
+    }
+};
